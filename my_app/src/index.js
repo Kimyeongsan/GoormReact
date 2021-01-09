595c10b1f67fb045ef6c1eb2ff7bb2f440492e0e
Kimyeongsan <apua34@gmail.com>
2021-01-09 21:51:39 +0900

    3강 수강 완료

--- my_app/src/index.js
+++ my_app/src/index.js
@@ -2,37 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 
 // JS code
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// JSX code
-// 2강 강의 내용
-
-const formatName = function(name) {
-  return name.firstName + " " + name.SecondName;
-}
-
-const name = {
-  firstName : "Kim",
-  SecondName : "San",
-};
-
-const element = <h1>Hello, {formatName(name)}!</h1>
-
 ReactDOM.render(
-  element,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// JSX code
+// 2강 강의 내용
+// const formatName = function(name) {
+//   return name.firstName + " " + name.SecondName;
+// }
+
+// const name = {
+//   firstName : "Kim",
+//   SecondName : "San",
+// };
+
+// const element = <h1>Hello, {formatName(name)}!</h1>
+
+// 3강 강의 내용
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Data().toLocaleTimeString()}.</h2>
+    </div>
+  );
+
+  ReactDOM.render(
+    element,
+    document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+
+serviceWorker.unregister();
